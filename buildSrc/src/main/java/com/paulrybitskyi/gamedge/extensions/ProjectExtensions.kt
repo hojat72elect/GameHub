5061b428094e116e835b281b669321093084d5c3
hojat72elect <hojat72elect@gmail.com>
2025-11-20 00:02:26 -0500

    Initial commit.Changed Gradle Project extension so it reads local.properties first.

--- buildSrc/src/main/java/com/paulrybitskyi/gamedge/extensions/ProjectExtensions.kt
+++ buildSrc/src/main/java/com/paulrybitskyi/gamedge/extensions/ProjectExtensions.kt
@@ -18,11 +18,19 @@ package com.paulrybitskyi.gamedge.extensions
 
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.the
+import java.util.Properties
 
 val Project.libs
     get() = the<org.gradle.accessors.dm.LibrariesForLibs>()
 
 @Suppress("UNCHECKED_CAST")
 fun <T> Project.property(key: String, default: T): T {
-    return ((properties[key] as? T) ?: default)
+    val localProperties = Properties()
+    val localPropertiesFile = rootProject.file("local.properties")
+
+    if (localPropertiesFile.exists()) {
+        localPropertiesFile.inputStream().use { localProperties.load(it) }
+    }
+
+    return ((localProperties[key] as? T) ?: (properties[key] as? T) ?: default)
 }
