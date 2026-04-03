d37b117849ddb40e24e0616b766668ccee1b3e87
hojat72elect <hojat72elect@gmail.com>
2026-04-02 22:15:52 -0400

    Added 2 more bottom tabs.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -30,6 +30,20 @@ export default function TabLayout() {
                     tabBarIcon: ({color}) => <IconSymbol size={28} name="paperplane.fill" color={color}/>,
                 }}
             />
+            <Tabs.Screen
+                name="news"
+                options={{
+                    title: 'News',
+                    tabBarIcon: ({color}) => <IconSymbol size={28} name="paperplane.fill" color={color}/>,
+                }}
+            />
+            <Tabs.Screen
+                name="settings"
+                options={{
+                    title: 'Settings',
+                    tabBarIcon: ({color}) => <IconSymbol size={28} name="paperplane.fill" color={color}/>,
+                }}
+            />
         </Tabs>
     );
 }
---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from 'react-native';
+
+export default function SettingsScreen() {
+    return (
+        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+            <Text>Hello from SettingsScreen</Text>
+        </View>
+    );
+}
