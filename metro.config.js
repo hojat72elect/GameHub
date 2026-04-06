924763caec3babf6e53bbbb5ed65ffacf08fb0f0
hojat72elect <hojat72elect@gmail.com>
2026-04-06 14:42:05 -0400

    Added required dependencies and configurations for using SVG icons.

--- metro.config.js
+++ metro.config.js
@@ -2,5 +2,7 @@ const {getDefaultConfig} = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Add any additional Metro configuration here if needed
+// SVG file support
+config.resolver.assetExts.push('svg');
+
 module.exports = config;
