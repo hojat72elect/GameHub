bf34b8ee4e35a0bfd8b6da519be6227c511b11b6
hojat72elect <hojat72elect@gmail.com>
2026-04-07 12:42:45 -0400

    Added better bottom tab bar icons.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,12 @@
 import {Tabs} from 'expo-router';
 import React from 'react';
 
+import SettingsIcon from '@/assets/svg/cog_outline.svg';
+import DiscoverIcon from '@/assets/svg/compass_rose.svg';
+import LikesIcon from '@/assets/svg/heart.svg';
+import NewsIcon from '@/assets/svg/newspaper.svg';
+
 import {HapticTab} from '@/components/haptic-tab';
-import {IconSymbol} from '@/components/ui/icon-symbol';
 import {Colors} from '@/constants/theme';
 import {useColorScheme} from '@/hooks/use-color-scheme';
 
@@ -20,28 +24,28 @@ export default function TabLayout() {
                 name="index"
                 options={{
                     title: 'Discover',
-                    tabBarIcon: ({color}) => <IconSymbol size={28} name="house.fill" color={color}/>,
+                    tabBarIcon: ({color}) => <DiscoverIcon width={28} height={28} fill={color}/>,
                 }}
             />
             <Tabs.Screen
                 name="likes"
                 options={{
                     title: 'Likes',
-                    tabBarIcon: ({color}) => <IconSymbol size={28} name="paperplane.fill" color={color}/>,
+                    tabBarIcon: ({color}) => <LikesIcon width={28} height={28} fill={color}/>,
                 }}
             />
             <Tabs.Screen
                 name="news"
                 options={{
                     title: 'News',
-                    tabBarIcon: ({color}) => <IconSymbol size={28} name="paperplane.fill" color={color}/>,
+                    tabBarIcon: ({color}) => <NewsIcon width={28} height={28} fill={color}/>,
                 }}
             />
             <Tabs.Screen
                 name="settings"
                 options={{
                     title: 'Settings',
-                    tabBarIcon: ({color}) => <IconSymbol size={28} name="paperplane.fill" color={color}/>,
+                    tabBarIcon: ({color}) => <SettingsIcon width={28} height={28} fill={color}/>,
                 }}
             />
         </Tabs>
