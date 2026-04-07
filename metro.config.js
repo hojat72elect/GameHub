daaf44b00848faecbf3174e89cd951cd50eec7fc
hojat72elect <hojat72elect@gmail.com>
2026-04-07 12:35:20 -0400

    Added required SVG configurations to metro.

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,13 @@ const {getDefaultConfig} = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// SVG file support
-config.resolver.assetExts.push('svg');
+// Remove SVG from asset extensions because it will be treated as a React Component
+config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== "svg");
+
+// Add SVG to source extensions so we can import it easily (just like a React component)
+config.resolver.sourceExts.push("svg");
+
+// Use our 3rd party transformer for handling SVG files
+config.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer");
 
 module.exports = config;
