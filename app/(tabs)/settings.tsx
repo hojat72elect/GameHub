7ebad0d14e5d49531cccab4e0501b75d8b892923
hojat72elect <hojat72elect@gmail.com>
2026-04-11 20:28:28 -0400

    Extracted SettingsScreen to a separate screen.

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,43 +1,8 @@
-import {ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {SettingsScreen} from "@/src/SettingsScreen";
 
-export default function SettingsScreen() {
+export default function TabFourScreen() {
     return (
-        <ScrollView contentContainerStyle={{padding: 20}}>
-            <Text style={{
-                fontSize: 28,
-                fontFamily: 'System',
-                marginBottom: 30,
-            }}>Settings</Text>
-
-            <View style={styles.section}>
-                <Text style={styles.sectionHeader}>Appearance</Text>
-                <TouchableOpacity style={styles.item}>
-                    <Text style={styles.itemLabel}>Theme</Text>
-                    <Text style={styles.itemValue}>Dark</Text>
-                </TouchableOpacity>
-            </View>
-
-            <View style={styles.section}>
-                <Text style={styles.sectionHeader}>About</Text>
-                <TouchableOpacity style={styles.item}>
-                    <Text style={styles.itemLabel}>Source Code</Text>
-                    <Text style={styles.itemValue}>View the source code of the app</Text>
-                </TouchableOpacity>
-                <View style={styles.item}>
-                    <Text style={styles.itemLabel}>Version</Text>
-                    <Text style={styles.itemValue}>v0.0.1-debug</Text>
-                </View>
-            </View>
-
-        </ScrollView>
+        <SettingsScreen/>
     );
 }
 
-
-const styles = StyleSheet.create({
-    section: {marginBottom: 25},
-    sectionHeader: {fontSize: 18, color: "#000", marginBottom: 10, fontFamily: "System"},
-    item: {marginBottom: 15},
-    itemLabel: {fontSize: 18, fontWeight: "500"},
-    itemValue: {fontSize: 16, marginTop: 2},
-});
