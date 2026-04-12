6260874dae70f23b21f778434c1d3b843de8224f
hojat72elect <hojat72elect@gmail.com>
2026-04-11 20:10:54 -0400

    Extracted LikesScreen to a separate component.

--- app/(tabs)/likes.tsx
+++ app/(tabs)/likes.tsx
@@ -1,9 +1,7 @@
-import {Text, View} from 'react-native';
+import {LikesScreen} from "@/src/LikesScreen";
 
 export default function TabTwoScreen() {
     return (
-        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-            <Text>Hello from Likes Screen</Text>
-        </View>
+        <LikesScreen/>
     );
 }
---
+++ src/LikesScreen.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from "react-native";
+import React from "react";
+
+export function LikesScreen(): React.JSX.Element {
+    return (
+        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+            <Text>Hello from new Likes Screen</Text>
+        </View>
+    );
+}
