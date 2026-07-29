4f613ee539c8cece2a363f447c8ed53564de51d1
hojat72elect <hojat72elect@gmail.com>
2026-07-28 21:49:00 -0400

    Fixed an error which I had caused myself 😅

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,8 @@ import {LikesProvider} from "@/src/feature_likes/LikesContext";
 
 export default function RootLayout() {
     return (
-        <ThemeProvider> // The information about the theme of the app
-            <LikesProvider> // The information about the games that are liked by the user
+        <ThemeProvider>{/*The information about the theme of the app*/}
+            <LikesProvider>{/*The information about the games that are liked by the user*/}
                 <Stack screenOptions={{headerShown: false}}/>
             </LikesProvider>
         </ThemeProvider>
