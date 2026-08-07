10cccf36a0a780c472f021cbb4d4ce602b3366e0
hojat72elect <hojat72elect@gmail.com>
2026-08-07 17:48:20 -0400

    Provided the language information as a context to the whole application.
    #14

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,16 @@
 import {Stack} from 'expo-router';
 import {ThemeProvider} from '@/src/ThemeContext';
 import {LikesProvider} from "@/src/feature_likes/LikesContext";
+import {LanguageProvider} from "@/src/LanguageContext";
+import '@/src/shared/internationalization';
 
 export default function RootLayout() {
     return (
         <ThemeProvider>{/*The information about the theme of the app*/}
             <LikesProvider>{/*The information about the games that are liked by the user*/}
-                <Stack screenOptions={{headerShown: false}}/>
+                <LanguageProvider>
+                    <Stack screenOptions={{headerShown: false}}/>
+                </LanguageProvider>
             </LikesProvider>
         </ThemeProvider>
     );
